b906b1cebb39dd2aabe980d967f151252e2ae885
Akamksha K <Akamksha.K@Honeywell.com>
2022-02-02 19:34:09 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const cors = require("cors");
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("DB connection successful")
+})
+.catch((err)=>{
+    console.log(`Error connecting DB ${err}`)
+
+});
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.listen(process.env.PORT || 5000, () =>{
+
+    console.log("backend server is running at port 5000!");
+});
